083109da46f66597afe2e71a5553275b550ca614
Shravan Manerikar <manerikarshravan@gmail.com>
2026-01-25 07:32:56 +0530

    frontend: nextjs configuration

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+};
+
+module.exports = nextConfig;
+ 
