115cc99a00e4f31adb8ddff5979717cbb18426b8
Shravan Manerikar <manerikarshravan@gmail.com>
2026-02-16 08:58:30 +0530

    style: format code components

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import login, clients, visits, ledger, services
+from app.api.api_v1.endpoints import login, clients, visits, ledger, services, dashboard
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
